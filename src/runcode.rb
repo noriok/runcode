fd68ae6805a7abadd28b0665e0600e734ef914fd
Norio Kobayashi <noriokob@gmail.com>
2015-10-24 23:25:49 +0900

    add -r:System.Numerics option

--- src/runcode.rb
+++ src/runcode.rb
@@ -8,7 +8,7 @@ CommandMap = {
   },
 
   '.cs' => {
-    :compile => 'mcs -debug %% -out:a.exe',
+    :compile => 'mcs -r:System.Numerics -debug %% -out:a.exe',
     :execute => 'mono --debug a.exe',
   },
 
